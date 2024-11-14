9c3132c673c1901db8a3e443645246458c92fd08
Ahmed Amer <ahmedamerworks@gmail.com>
2024-11-14 14:51:44 -0500

    added DTOs, implemented JsonParser and tested successfully

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/practice/APITest.java
@@ -0,0 +1,35 @@
+package ca.jrvs.apps.practice;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class APITest {
+    final static String symbol = "MSFT";
+    final static String apiKey = "ZCN7WDQ6ZKX8V3HC";
+    public static void main(String[] args) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json"))
+                .header("X-RapidAPI-Key", apiKey)
+                .header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
